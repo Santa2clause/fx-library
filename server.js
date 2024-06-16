0dffe827bad0312c34622cbec4fdb71f8b5b20e6
Santa2clause <corne.coetzee@rocketmail.com>
2024-06-16 12:32:17 +0200

    Added Cron Job

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const pool = require('./db');
 const cors = require('cors');
+const cron = require('node-cron');
+
 const { fetchRates } = require('./api/fetch-rates');
 const { storeRates } = require('./api/store-data');
 const { getTodaysRates } = require('./api/get-today-rate');
@@ -34,6 +36,23 @@ app.get('/get-currency-codes', getCurrencyCodes);
 app.get('/insert-currency-codes', insertCurrencyCodes);
 
 app.get('/closeValue', getCloseValue);
+
+cron.schedule('0 12 * * *', async () => {
+  try {
+    console.log('Running daily fetch rates job at 12:00 PM SAST');
+    const rates = await fetchRates(process.env.API_ACCESS_KEY);
+    if (rates) {
+      await storeRates(rates);
+      console.log('Rates fetched and stored successfully');
+    } else {
+      console.error('Failed to fetch rates');
+    }
+  } catch (error) {
+    console.error('Error during scheduled fetch rates job:', error.message);
+  }
+}, {
+  timezone: "Africa/Johannesburg"
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
